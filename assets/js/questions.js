951ec90598482de2aff72126efe35520b59feca1
Travis <travis3214@yahoo.com>
2023-07-07 16:20:13 -0400

    added some questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,8 +1,28 @@
 var questions = [
     {
-        q: ""
-        a: ""
-        c: ""
+        q: "A # on your CSS page will call on the ____ you wish to style.",
+        a: ["Class", "ID", "JavScript", "Numbers"],
+        c: "ID"
+    },
+    {
+        q: "In JavaScript, how would you access the second item in an array named 'exArray'?",
+        a: ["exArray[0]", "exArray[1]", "exArray[2]", "exArray(2)" ],
+        c: "exArray[1]"
+    },
+    {
+        q: "Data types include all of the following EXCEPT ____",
+        a: ["Strings", "Alerts", "Numbers", "Booleans"],
+        c: "Alerts"
+    },
+    {
+        q: "",
+        a: ["Answer choices go here"],
+        c: "Correct answer goes here"
+    },
+    {
+        q: "Questions go here:",
+        a: ["Answer choices go here"],
+        c: "Correct answer goes here"
     },
 
-]
+];
