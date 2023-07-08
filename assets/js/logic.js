9b86dfb56503e0a32d23333163fec2eece788848
Travis <travis3214@yahoo.com>
2023-07-07 22:15:05 -0400

    added variables to logic page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,23 +1,10 @@
-//function to start the quiz on the click of the start button
-
-//start and display the first question
-
-//think of the elements that you need to target when listening for the answer event and whats classes and id's they have
-
-//logic to detect if answer is correct and apply the time deduction if incorrect or move onto the next question if correct
-
-//loop back to the question function until all questions are answered or the timer reaches 0
-
-//arrays of objects
-
-//Q: "question string"
-//A1:
-//A2:
-//A3:
-//A4:
-//CA:
-
-//stop to the question function log score - read the interval value and request intials from user
-
-//write response and time remaining value to local storage
-
+var questionIndex = 0;
+var score = 0;
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var secondsLeft = 90;
+var holdInterval = 0;
+var questionsDiv = document.querySelector("#questionsDiv");
+var ulCreate = document.createElement("ul");
+var penalty = 10;
+var wrapper = document.querySelector("#wrapper");
