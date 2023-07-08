38373f7bb9f325c94bb3f12bc0da1f4bed572d0c
Travis <travis3214@yahoo.com>
2023-07-07 22:23:00 -0400

    set up the timer and start for the quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,3 +8,21 @@ var questionsDiv = document.querySelector("#questionsDiv");
 var ulCreate = document.createElement("ul");
 var penalty = 10;
 var wrapper = document.querySelector("#wrapper");
+
+//This starts the timer and displays the questions on screen//
+timer.addEventListener("click", function () {
+
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
