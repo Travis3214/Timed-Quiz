243e9c2570acaa8a623c5409abf10e8752caa2b2
Travis <travis3214@yahoo.com>
2023-07-07 22:27:09 -0400

    added function to display questions when quiz starts

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -26,3 +26,24 @@ timer.addEventListener("click", function () {
     }
     render(questionIndex);
 });
+
+//This is the function to render the questions and answer choices onto the page//
+function render(questionIndex) {
+
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionIndex].q;
+        var userChoices = questions[questionIndex].a;
+        questionsDiv.textContent = userQuestion;
+    }
+
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
